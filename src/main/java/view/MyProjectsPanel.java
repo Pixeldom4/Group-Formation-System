035fc20061ed9bf1bbf57839cf3a7ff0fbccd777
Arthur <artam9310@gmail.com>
2024-07-11 13:28:37 -0400

    ADD: view for scroll panel for the projects

--- src/main/java/view/MyProjectsPanel.java
+++ src/main/java/view/MyProjectsPanel.java
@@ -3,6 +3,10 @@ package view;
 import viewmodel.MyProjectsPanelViewModel;
 
 import javax.swing.*;
+import javax.swing.event.ListSelectionEvent;
+import javax.swing.event.ListSelectionListener;
+import javax.swing.table.TableColumn;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
@@ -12,7 +16,7 @@ public class MyProjectsPanel extends JPanel implements ActionListener, PropertyC
 
     private final MyProjectsPanelViewModel myProjectsPanelViewModel;
 
-    private final JLabel title = new JLabel("My Projects");
+//    private final JLabel title = new JLabel("My Projects");
 
     public MyProjectsPanel(MyProjectsPanelViewModel myProjectsPanelViewModel) {
         this.myProjectsPanelViewModel = myProjectsPanelViewModel;
@@ -22,11 +26,39 @@ public class MyProjectsPanel extends JPanel implements ActionListener, PropertyC
         Object[][] data = {{"project1", "test desc", "bob, john, tim"}};
 
         JTable table = new JTable(data, columnTitles);
-//        table.getTableHeader().setBounds(50, 30, 700, 20);
-//        table.setCellSelectionEnabled(true);
-        this.add(title);
-        this.add(table.getTableHeader());
-        this.add(table);
+
+        table.setCellSelectionEnabled(true);
+        ListSelectionModel select = table.getSelectionModel();
+        select.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+        select.addListSelectionListener(new ListSelectionListener() {
+            @Override
+            public void valueChanged(ListSelectionEvent e) {
+                String data = null;
+
+                for (int row : table.getSelectedRows()){
+                    for (int column : table.getSelectedColumns()){
+                        data = (String)table.getValueAt(row, column);
+                    }
+                }
+
+                System.out.println(data);
+            }
+        });
+
+        JScrollPane scrollPane = new JScrollPane(table);
+        table.setFillsViewportHeight(true);
+
+        TableColumn column = null;
+        for(int i = 0; i < 3; i++){
+            column = table.getColumnModel().getColumn(i);
+            if(i == 0){
+                column.setPreferredWidth(100);
+            }else{
+                column.setPreferredWidth(50);
+            }
+        }
+
+        this.add(scrollPane);
     }
 
     @Override
