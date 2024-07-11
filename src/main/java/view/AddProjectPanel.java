c86a1fed252e1faea44d92951f99e4df2e94d7b9
raqk <luke.ching@mail.utoronto.ca>
2024-07-11 19:14:34 -0400

    add components to AddProjectPanel

--- src/main/java/view/AddProjectPanel.java
+++ src/main/java/view/AddProjectPanel.java
@@ -3,22 +3,114 @@ package view;
 import viewmodel.AddProjectPanelViewModel;
 
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
+import javax.swing.text.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
+import java.util.ArrayList;
 
 public class AddProjectPanel extends JPanel implements ActionListener, PropertyChangeListener {
 
     private final AddProjectPanelViewModel addProjectPanelViewModel;
 
-    private final JLabel testLabel = new JLabel("Test label");
+    private final JPanel projectInfoPanel = new JPanel();
+    private final JPanel projectDataPanel = new JPanel();
+    private final GridLayout projectDataGridLayout = new GridLayout(0, 2);
+    private final JLabel projectNameLabel = new JLabel("Project Name");
+    private final JLabel projectBudgetLabel = new JLabel("Project Budget");
+    private final JLabel projectDescriptionLabel = new JLabel("Project Description");
+    private final JLabel projectTagsLabel = new JLabel("Project Tags");
+    private final JTextField projectNameField = new JTextField();
+    private final JTextField projectBudgetField = new NumericTextField();
+    private final JTextField projectDescriptionField = new JTextField();
+
+    private final JTextField projectTagsField = new JTextField();
+    private final JButton addTagButton = new JButton("Add Tag");
+    private final GridLayout tagPanelLayout = new GridLayout(0, 1);
+    private final JPanel tagPanel = new JPanel();
+    private final JLabel tagPanelLabel = new JLabel("Project tags: ");
+
+    private final JButton addProjectButton = new JButton("Create project");
+
+    private final ArrayList<String> tags = new ArrayList<>();
 
     public AddProjectPanel(AddProjectPanelViewModel addProjectPanelViewModel) {
         this.addProjectPanelViewModel = addProjectPanelViewModel;
         addProjectPanelViewModel.addPropertyChangeListener(this);
 
-        this.add(testLabel);
+        this.setLayout(new BoxLayout(this, BoxLayout.Y_AXIS));
+        projectInfoPanel.setLayout(new BoxLayout(projectInfoPanel, BoxLayout.Y_AXIS));
+
+        addTagButton.addActionListener(e -> {
+            String tagText = projectTagsField.getText();
+            if (!tagText.isEmpty()) {
+                tags.add(tagText);
+                addTagToPanel(tagText);
+                projectTagsField.setText("");
+            }
+        });
+
+        projectDataPanel.setLayout(projectDataGridLayout);
+
+        projectDataPanel.add(projectNameLabel);
+        projectDataPanel.add(projectNameField);
+        projectDataPanel.add(projectBudgetLabel);
+        projectDataPanel.add(projectBudgetField);
+        projectDataPanel.add(projectDescriptionLabel);
+        projectDataPanel.add(projectDescriptionField);
+        projectDataPanel.add(projectTagsLabel);
+        projectDataPanel.add(projectTagsField);
+        projectDataPanel.add(addTagButton);
+
+        projectInfoPanel.add(projectDataPanel);
+
+        tagPanel.add(tagPanelLabel);
+        tagPanel.setLayout(tagPanelLayout);
+        tagPanel.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        projectInfoPanel.add(tagPanel);
+
+        this.add(projectInfoPanel);
+        this.add(addProjectButton);
+
+    }
+
+    private void addTagToPanel(String text) {
+        JLabel label = new JLabel(text);
+        label.setOpaque(true);
+        label.setBackground(Color.LIGHT_GRAY);
+        label.setBorder(BorderFactory.createEmptyBorder(5, 5, 5, 5));
+        JButton removeButton = new JButton("x");
+
+        JPanel tag = new JPanel();
+        tag.setBorder(new EmptyBorder(0, 10, 0, 10));
+        tag.setLayout(new GridBagLayout());
+
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.HORIZONTAL;
+        constraints.gridy = 0;
+
+        constraints.gridx = 0;
+        constraints.weightx = 0.8;
+        tag.add(label, constraints);
+
+        constraints.gridx = 1;
+        constraints.weightx = 0.2;
+        tag.add(removeButton, constraints);
+
+        removeButton.addActionListener(e -> {
+            tags.remove(text);
+            tagPanel.remove(tag);
+            tagPanel.revalidate();
+            tagPanel.repaint();
+        });
+
+        tagPanel.add(tag);
+        tagPanel.revalidate();
+        tagPanel.repaint();
     }
 
     @Override
@@ -30,4 +122,28 @@ public class AddProjectPanel extends JPanel implements ActionListener, PropertyC
     public void propertyChange(PropertyChangeEvent evt) {
 
     }
+
+
+    private class NumericTextField extends JTextField {
+        public NumericTextField() {
+            super(10);
+            ((AbstractDocument) this.getDocument()).setDocumentFilter(new DocumentFilter() {
+                @Override
+                public void insertString(FilterBypass fb, int offset, String string, AttributeSet attr) throws BadLocationException {
+                    if (string.matches("\\d+")) {
+                        super.insertString(fb, offset, string, attr);
+                    }
+                }
+
+                @Override
+                public void replace(FilterBypass fb, int offset, int length, String text, AttributeSet attrs) throws BadLocationException {
+                    if (text.matches("\\d+")) {
+                        super.replace(fb, offset, length, text, attrs);
+                    }
+                }
+            });
+        }
+
+    }
+
 }
