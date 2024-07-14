18a8f82b8d34648cd1d230ee47061e721b9a9160
cecexyzhang <cecexyzhang@gmail.com>
2024-07-13 21:00:02 -0400

    added SearchProjectByIdInteractor

--- src/main/java/usecase/searchprojectbyid/LocalProjectSearchByIdObject.java
+++ src/main/java/usecase/searchprojectbyid/LocalProjectSearchByIdObject.java
@@ -8,11 +8,14 @@ import entities.ProjectInterface;
 public class LocalProjectSearchByIdObject implements SearchProjectByIdInterface {
     private IProjectRepository projectDataAccess = DAOImplementationConfig.getProjectRepository();
 
+    /**
+     * Creates a new LocalProjectSearchByIdObject.
+     */
     public LocalProjectSearchByIdObject() {
     }
 
     /**
-     * Creates a new LocalProjectSearchObject using the given project repository.
+     * Creates a new LocalProjectSearchByIdObject using the given project repository.
      * Used for testing where files are not stored in the project folder.
      *
      * @param projectRepository The project repository to use.
@@ -21,6 +24,11 @@ public class LocalProjectSearchByIdObject implements SearchProjectByIdInterface 
         projectDataAccess = projectRepository;
     }
 
+    /**
+     * Searches for a project by its ID.
+     *
+     * @param projectId The ID of the project to search for.
+     */
     @Override
     public ProjectInterface searchProjectById(int projectId) {
         Project result = projectDataAccess.getProjectById(projectId);
