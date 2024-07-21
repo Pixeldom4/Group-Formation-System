7818249f062b6ebc85d93b0b6cb5ec500a8f01a4
cecexyzhang <cecexyzhang@gmail.com>
2024-07-20 20:06:51 -0400

    added DeleteApplicationInputBoundary

---
+++ src/main/java/usecase/deleteapplication/DeleteApplicationInputBoundary.java
@@ -0,0 +1,13 @@
+package usecase.deleteapplication;
+
+/**
+ * The input boundary for deleting application use case.
+ */
+public interface DeleteApplicationInputBoundary {
+    /**
+     * Deletes an application with the provided input data.
+     *
+     * @param inputData the input data required to delete the application.
+     */
+    void deleteApplication(DeleteApplicationInputData inputData);
+}
