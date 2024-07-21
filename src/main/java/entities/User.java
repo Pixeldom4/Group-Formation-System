17d9b1da9f5dfac5bc0cb78da7ad236c60235cad
Sherwin Okhowat <okthatssherwin@gmail.com>
2024-07-21 16:41:33 -0400

    javadoc for entites package

--- src/main/java/entities/User.java
+++ src/main/java/entities/User.java
@@ -2,6 +2,9 @@ package entities;
 
 import java.util.HashSet;
 
+/**
+ * Represents a user entity with details about the user.
+ */
 public class User implements UserInterface {
     private int userId;
     private String firstName;
@@ -10,7 +13,16 @@ public class User implements UserInterface {
     private HashSet<String> tags;
     private double desiredCompensation;
 
-    // Constructor
+    /**
+     * Constructs a User object with the specified details.
+     *
+     * @param userId the ID of the user.
+     * @param firstName the first name of the user.
+     * @param lastName the last name of the user.
+     * @param userEmail the email address of the user.
+     * @param tags a set of tags associated with the user.
+     * @param desiredCompensation the desired compensation of the user.
+     */
     public User(int userId, String firstName, String lastName, String userEmail, HashSet<String> tags, double desiredCompensation) {
         this.userId = userId;
         this.firstName = firstName;
@@ -20,51 +32,110 @@ public class User implements UserInterface {
         this.desiredCompensation = desiredCompensation;
     }
 
-    // Getters and Setters
+    /**
+     * Gets the user ID.
+     *
+     * @return the user ID.
+     */
     public int getUserId() {
         return userId;
     }
 
+    /**
+     * Sets the user ID.
+     *
+     * @param userId the user ID.
+     */
     public void setUserId(int userId) {
         this.userId = userId;
     }
 
+    /**
+     * Gets the first name of the user.
+     *
+     * @return the first name of the user.
+     */
     public String getFirstName() {
         return this.firstName;
     }
 
+    /**
+     * Sets the first name of the user.
+     *
+     * @param firstName the first name of the user.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Gets the last name of the user.
+     *
+     * @return the last name of the user.
+     */
     public String getLastName() {
         return this.lastName;
     }
 
+    /**
+     * Sets the last name of the user.
+     *
+     * @param lastName the last name of the user.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Gets the email address of the user.
+     *
+     * @return the email address of the user.
+     */
     public String getUserEmail() {
         return userEmail;
     }
 
+    /**
+     * Sets the email address of the user.
+     *
+     * @param userEmail the email address of the user.
+     */
     public void setUserEmail(String userEmail) {
         this.userEmail = userEmail;
     }
 
+    /**
+     * Gets the tags associated with the user.
+     *
+     * @return a set of tags associated with the user.
+     */
     public HashSet<String> getTags() {
         return tags;
     }
 
+    /**
+     * Sets the tags associated with the user.
+     *
+     * @param tags a set of tags associated with the user.
+     */
     public void setTags(HashSet<String> tags) {
         this.tags = tags;
     }
 
+    /**
+     * Gets the desired compensation of the user.
+     *
+     * @return the desired compensation of the user.
+     */
     public double getDesiredCompensation() {
         return desiredCompensation;
     }
 
+    /**
+     * Sets the desired compensation of the user.
+     *
+     * @param desiredCompensation the desired compensation of the user.
+     */
     public void setDesiredCompensation(double desiredCompensation) {
         this.desiredCompensation = desiredCompensation;
     }
--- src/main/java/entities/UserInterface.java
+++ src/main/java/entities/UserInterface.java
@@ -2,19 +2,92 @@ package entities;
 
 import java.util.HashSet;
 
+/**
+ * Interface for the User entity.
+ */
 public interface UserInterface {
 
+    /**
+     * Gets the user ID.
+     *
+     * @return the user ID.
+     */
     int getUserId();
+
+    /**
+     * Gets the first name of the user.
+     *
+     * @return the first name of the user.
+     */
     String getFirstName();
+
+    /**
+     * Gets the last name of the user.
+     *
+     * @return the last name of the user.
+     */
     String getLastName();
+
+    /**
+     * Gets the email address of the user.
+     *
+     * @return the email address of the user.
+     */
     String getUserEmail();
+
+    /**
+     * Gets the tags associated with the user.
+     *
+     * @return a set of tags associated with the user.
+     */
     HashSet<String> getTags();
+
+    /**
+     * Gets the desired compensation of the user.
+     *
+     * @return the desired compensation of the user.
+     */
     double getDesiredCompensation();
 
+    /**
+     * Sets the user ID.
+     *
+     * @param userId the user ID.
+     */
     void setUserId(int userId);
+
+    /**
+     * Sets the first name of the user.
+     *
+     * @param firstName the first name of the user.
+     */
     void setFirstName(String firstName);
+
+    /**
+     * Sets the last name of the user.
+     *
+     * @param lastName the last name of the user.
+     */
     void setLastName(String lastName);
+
+    /**
+     * Sets the email address of the user.
+     *
+     * @param userEmail the email address of the user.
+     */
     void setUserEmail(String userEmail);
+
+    /**
+     * Sets the tags associated with the user.
+     *
+     * @param tags a set of tags associated with the user.
+     */
     void setTags(HashSet<String> tags);
+
+    /**
+     * Sets the desired compensation of the user.
+     *
+     * @param desiredCompensation the desired compensation of the user.
+     */
     void setDesiredCompensation(double desiredCompensation);
 }
