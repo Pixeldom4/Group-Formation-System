19957e39966cb7ada864bda213442fd0e83bbb18
cecexyzhang <cecexyzhang@gmail.com>
2024-07-20 20:08:43 -0400

    added DeleteApplicationOutputBoundary

---
+++ src/main/java/usecase/deleteapplication/DeleteApplicationOutputBoundary.java
@@ -0,0 +1,20 @@
+package usecase.deleteapplication;
+
+/**
+ * The output boundary for the Delete Application use case.
+ */
+public interface DeleteApplicationOutputBoundary {
+    /**
+     * Prepares the success view with the provided output data.
+     *
+     * @param outputData the output data to present in case of success.
+     */
+    void prepareSuccessView(DeleteApplicationOutputData outputData);
+
+    /**
+     * Prepares the failure view with the provided error message.
+     *
+     * @param error the error message to present in case of failure.
+     */
+    void prepareFailView(String error);
+}
