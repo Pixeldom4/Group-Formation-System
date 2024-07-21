8133ca243074756c2c639ec819a6f72a7d21aea0
cecexyzhang <cecexyzhang@gmail.com>
2024-07-21 18:31:09 -0400

    added remaining javadoc to the entities package

--- src/main/java/entities/Group.java
+++ src/main/java/entities/Group.java
@@ -2,39 +2,77 @@ package entities;
 
 import java.util.HashSet;
 
+/**
+ * Represents a group of users associated with a project.
+ */
 public class Group implements GroupInterface {
     private HashSet<User> users;
     private String groupName;
     private Project project;
 
-    // Constructor
+    /**
+     * Constructs a Group with the specified users, group name, and project.
+     *
+     * @param users the set of users in the group
+     * @param groupName the name of the group
+     * @param project the project associated with the group
+     */
     public Group(HashSet<User> users, String groupName, Project project) {
         this.users = users;
         this.groupName = groupName;
         this.project = project;
     }
 
-    // Getters and Setters
+    /**
+     * Returns the set of users in the group.
+     *
+     * @return the set of users
+     */
     public HashSet<User> getUsers() {
         return users;
     }
 
+    /**
+     * Sets the users in the group.
+     *
+     * @param users the set of users to be set
+     */
     public void setUsers(HashSet<User> users) {
         this.users = users;
     }
 
+    /**
+     * Returns the name of the group.
+     *
+     * @return the name of the group
+     */
     public String getGroupName() {
         return groupName;
     }
 
+    /**
+     * Sets the name of the group.
+     *
+     * @param groupName the name of the group to be set
+     */
     public void setGroupName(String groupName) {
         this.groupName = groupName;
     }
 
+    /**
+     * Returns the project associated with the group.
+     *
+     * @return the project
+     */
     public Project getProject() {
         return project;
     }
 
+    /**
+     * Sets the project associated with the group.
+     *
+     * @param project the project to be set
+     */
     public void setProject(Project project) {
         this.project = project;
     }
--- src/main/java/entities/GroupInterface.java
+++ src/main/java/entities/GroupInterface.java
@@ -2,13 +2,50 @@ package entities;
 
 import java.util.HashSet;
 
+/**
+ * Interface representing a group of users associated with a project.
+ */
 public interface GroupInterface {
 
+    /**
+     * Returns the set of users in the group.
+     *
+     * @return the set of users
+     */
     HashSet<User> getUsers();
+
+    /**
+     * Returns the name of the group.
+     *
+     * @return the name of the group
+     */
     String getGroupName();
+
+    /**
+     * Returns the project associated with the group.
+     *
+     * @return the project
+     */
     Project getProject();
 
+    /**
+     * Sets the users in the group.
+     *
+     * @param users the set of users to be set
+     */
     void setUsers(HashSet<User> users);
+
+    /**
+     * Sets the name of the group.
+     *
+     * @param groupName the name of the group to be set
+     */
     void setGroupName(String groupName);
+
+    /**
+     * Sets the project associated with the group.
+     *
+     * @param project the project to be set
+     */
     void setProject(Project project);
 }
