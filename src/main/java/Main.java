429f99361667cc3c21bd4dfc08d1b9934bb00694
raqk <luke.ching@mail.utoronto.ca>
2024-08-03 12:05:22 -0400

    add loading bar to main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -64,22 +64,26 @@ class Main {
             }
         });
 
+        // Print which data access implementation is used
+        System.out.println(DataAccessConfig.getImplementation());
+
+        printLoadingBar(70, "Starting app");
+
         CardLayout cardLayout = new CardLayout();
         JPanel views = new JPanel(cardLayout);
         application.add(views);
-
-        // Print which data access implementation is used
-        System.out.println(DataAccessConfig.getImplementation());
 
         ViewManagerModel viewManagerModel = new ViewManagerModel();
         ViewManager viewManager = new ViewManager(views, cardLayout, viewManagerModel);
 
         // Create User Panel
+        printLoadingBar(80, "Creating user panel");
         CreateUserPanelViewModel createUserPanelViewModel = new CreateUserPanelViewModel();
         CreateUserController createUserController = CreateUserUseCaseFactory.create(createUserPanelViewModel);
         CreateUserPanel createUserPanel = new CreateUserPanel(createUserPanelViewModel, createUserController);
 
         // Login Panel
+        printLoadingBar(86, "Creating login panel");
         LoginPanelViewModel loginPanelViewModel = new LoginPanelViewModel();
         LoginUserController loginUserController = LoginUserUseCaseFactory.create(loginPanelViewModel);
         LoginVerificationViewModel loginVerificationViewModel = new LoginVerificationViewModel();
@@ -91,31 +95,38 @@ class Main {
                                                createVerificationController);
 
         // Search Project Panel
+        printLoadingBar(98, "Creating search panel");
         SearchPanelViewModel searchPanelViewModel = new SearchPanelViewModel();
         SearchProjectController searchProjectController = SearchProjectUseCaseFactory.createSearchProjectController(searchPanelViewModel);
         GetLoggedInUserController searchPanelGetLoggedInUserController = GetLoggedInUserUseCaseFactory.create(searchPanelViewModel);
         CreateApplicationController createApplicationController = CreateApplicationUseCaseFactory.createController(searchPanelViewModel);
         SearchPanel searchPanel = new SearchPanel(viewManagerModel,searchPanelViewModel, searchProjectController, searchPanelGetLoggedInUserController, createApplicationController);
 
+
+
         // Add Project Panel
+        printLoadingBar(108, "Creating add project panel");
         AddProjectPanelViewModel addProjectPanelModel = new AddProjectPanelViewModel();
         CreateProjectController createProjectController = CreateProjectUseCaseFactory.createController(addProjectPanelModel);
         GetLoggedInUserController addProjectGetLoggedInUserController = GetLoggedInUserUseCaseFactory.create(addProjectPanelModel);
         AddProjectPanel addProjectPanel = new AddProjectPanel(viewManagerModel, addProjectPanelModel, createProjectController, addProjectGetLoggedInUserController);
 
         // My Projects Panel
+        printLoadingBar(115, "Creating my projects panel");
         MyProjectsPanelViewModel myProjectsViewModel = new MyProjectsPanelViewModel();
         GetLoggedInUserController getLoggedInUserController = GetLoggedInUserUseCaseFactory.create(myProjectsViewModel);
         GetProjectsController getProjectsController = GetProjectsUseCaseFactory.createGetProjectsController(myProjectsViewModel);
         DeleteProjectController deleteProjectController = DeleteProjectUseCaseFactory.createDeleteProjectController(myProjectsViewModel);
 
         // Display Project Application View
+        printLoadingBar(122, "Creating project app view");
         DisplayProjectApplicationViewModel displayProjectApplicationViewModel = new DisplayProjectApplicationViewModel();
         GetApplicationsController getApplicationsController = GetApplicationsUseCaseFactory.createController(displayProjectApplicationViewModel);
         AcceptApplicationController acceptApplicationController = AcceptApplicationUseCaseFactory.createController(displayProjectApplicationViewModel);
         RejectApplicationController rejectApplicationController = RejectApplicationUseCaseFactory.createController(displayProjectApplicationViewModel);
 
         // Edit Project Panel
+        printLoadingBar(130, "Creating edit project panel");
         EditProjectPanelViewModel editProjectPanelViewModel = new EditProjectPanelViewModel();
         EditProjectController editProjectController = EditProjectUseCaseFactory.createController(editProjectPanelViewModel);
         EditProjectPanel editProjectPanel = new EditProjectPanel(
@@ -138,6 +149,7 @@ class Main {
                 editProjectPanel);
 
         // Edit Profile Panel
+        printLoadingBar(152, "Creating edit profile panel");
         EditProfileViewModel editProfileViewModel = new EditProfileViewModel();
         EditUserController editUserController = EditUserUseCaseFactory.create(editProfileViewModel);
         GetLoggedInUserController myProfileGetLoggedInUserController = GetLoggedInUserUseCaseFactory.create(editProfileViewModel);
@@ -151,6 +163,7 @@ class Main {
         );
 
         // add views to card layout
+        printLoadingBar(166, "Adding views to card layout");
         views.add(createUserPanel, createUserPanelViewModel.getViewName());
         views.add(loginPanel, loginPanelViewModel.getViewName());
         views.add(searchPanel, searchPanelViewModel.getViewName());
@@ -158,6 +171,8 @@ class Main {
         views.add(myProjectsPanel, myProjectsViewModel.getViewName());
         views.add(editProfilePanel, editProfileViewModel.getViewName());
 
+        // Bottom panels (switch view and settings)
+        printLoadingBar(175, "Creating bottom panels");
         JPanel bottomPanel = new JPanel();
         bottomPanel.setLayout(new GridLayout(0, 1));
 
@@ -179,6 +194,29 @@ class Main {
 
         application.pack();
         application.setVisible(true);
+        System.out.println("Loading complete!");
 
+    }
+
+    /**
+     * Print a loading bar to the console
+     * @param n the line number
+     * @param taskMessage the message to display
+     */
+    private static void printLoadingBar(int n, String taskMessage) {
+        int width = 50;
+
+        // Progress based on line number
+        int start = 70;
+        int end = 197;
+        int prog = n - start;
+        int total = end - start;
+        int i = prog * width / total;
+
+        System.out.print("\r" + "Loading app" + ": [");
+        System.out.print("=".repeat(i));
+        System.out.print(" ".repeat(width - i));
+        System.out.print("] " + prog * 100 / total + "% ");
+        System.out.print(taskMessage + "\r");
     }
 }
