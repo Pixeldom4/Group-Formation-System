013e528e9cd0943504899cd40beda17a17305b1a
cecexyzhang <cecexyzhang@gmail.com>
2024-08-03 16:50:17 -0400

    added tests for DeleteApplicationPresenter

---
+++ src/test/java/usecase/deleteapplication/DeleteApplicationPresenterTest.java
@@ -0,0 +1,20 @@
+package usecase.deleteapplication;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * Unit tests for the DeleteApplicationPresenter class.
+ */
+public class DeleteApplicationPresenterTest {
+
+    /**
+     * Tests that the presenter displays a failure message when an error occurs during application deletion.
+     */
+    @Test
+    void deleteApplicationPresenterDisplaysFailMessage() {
+        String errorMessage = "Error deleting application";
+        DeleteApplicationPresenter presenter = new DeleteApplicationPresenter();
+        presenter.prepareFailView(errorMessage);
+        // Verify that the error message is displayed
+    }
+}
